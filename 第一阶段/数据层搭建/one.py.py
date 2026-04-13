2abc7fb7e5a4b9222569ceeb4c1937c4a4e77757
suqiyun68 <anicetstratman@gmail.com>
2026-04-13 21:29:16 +0800

    Add files via upload
    本阶段实现了量化交易系统的基础数据获取模块。通过 Tushare 金融数据接口，获取指定股票的历史日线行情数据，为后续的策略分析、回测与实盘交易提供可靠的数据源。
    本项目使用 Tushare Pro API 初始化数据接口，获取股票（示例：永兴材料，代码 002756.SZ）在指定时间段内的日线数据，输出数据的前几行，验证数据获取的正确性

---
+++ 第一阶段/数据层搭建/one.py.py
@@ -0,0 +1,11 @@
+import tushare as ts
+
+# 1. 初始化接口，替换 '你的token' 为刚才复制的真实token
+pro = ts.pro_api('你的token')
+
+# 2. 调用接口，获取贵州茅台（600519.SH）的日线数据
+df = pro.daily(ts_code='002756.SZ', start_date='20260320', end_date='20260413')
+
+# 3. 查看数据
+print("永兴材料")
+print(df.head())
